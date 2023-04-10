03323f5de9c8e4912c7004d2681c54cae8835cdf
Evgen Konyshock <e.konyshock@nexters.com>
2023-04-10 14:04:38 +0300

    Add different blocks

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class WorldGenerator : MonoBehaviour {
     public int width = 50;
     public int height = 50;
     public float scale = 10.0f;
     public float heightScale = 10.0f;
-    public GameObject blockPrefab;
+    [FormerlySerializedAs("dirtBlockPrefab")] public GameObject bottomBlockPrefab;
+    [FormerlySerializedAs("grassBlockPrefab")] public GameObject midBlockPrefab;
+    [FormerlySerializedAs("stoneBlockPrefab")] public GameObject topBlockPrefab;
 
     private void Start() {
         GenerateWorld();
@@ -17,6 +20,16 @@ public class WorldGenerator : MonoBehaviour {
                 float heightValue = Mathf.PerlinNoise((float)x / width * scale, (float)z / height * scale) * heightScale;
 
                 for (int y = 0; y < heightValue; y++) {
+                    GameObject blockPrefab;
+
+                    if (y == Mathf.FloorToInt(heightValue) - 1) {
+                        blockPrefab = midBlockPrefab;
+                    } else if (y < Mathf.FloorToInt(heightValue) - 1 && y >= Mathf.FloorToInt(heightValue) - 5) {
+                        blockPrefab = bottomBlockPrefab;
+                    } else {
+                        blockPrefab = topBlockPrefab;
+                    }
+
                     GameObject block = Instantiate(blockPrefab, new Vector3(x, y, z), Quaternion.identity);
                 }
             }
